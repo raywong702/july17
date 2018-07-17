495174a72a223c105e967c8b09752cf0f35f28c0
Samer Buna <samer.buna@gmail.com>
2018-07-17 14:30:53 -0400

    update

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement(
+    'h1',
+    null,
+    'Hello React Bundled'
+  ),
+  document.getElementById('root'),
+);
